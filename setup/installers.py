8fd56146e286ee1e981323a456d3a43e8bf78a09
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-18 16:11:54 +0530

    ...

--- setup/installers.py
+++ setup/installers.py
@@ -26,7 +26,7 @@ def get_exe():
     return 'python3' if sys.version_info.major == 2 else sys.executable
 
 
-def get_cmd(exe, bypy, which, bitness, sign_installers, notarize=True, compression_level='9', action='program'):
+def get_cmd(exe, bypy, which, bitness, sign_installers=False, notarize=True, compression_level='9', action='program'):
     cmd = [exe, bypy, which]
     if bitness and bitness != '64':
         cmd += ['--arch', bitness]
@@ -62,7 +62,7 @@ def build_only(which, bitness, spec, shutdown=False):
     dist = get_dist(base, which, bitness)
     dist = os.path.join(dist, 'c-extensions')
     if shutdown:
-        cmd = get_cmd(action='shutdown')
+        cmd = get_cmd(exe, bypy, which, bitness, action='shutdown')
         subprocess.Popen(cmd).wait()
     return dist
 
@@ -87,7 +87,7 @@ def build_single(which='windows', bitness='64', shutdown=True, sign_installers=T
             pass
         os.link(src, dest)
     if shutdown:
-        cmd = get_cmd(action='shutdown')
+        cmd = get_cmd(exe, bypy, which, bitness, action='shutdown')
         subprocess.Popen(cmd).wait()
 
 
